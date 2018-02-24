2e20b972dc3538e95a7787f52a4727214f56f733
U-CYSEMI\whao <whao@LAPTOP-WHAO.cysemi.com>
2018-02-24 08:41:38 +0800

    Initial commit for AliOS-Things with v1.2.1 code base.

---
+++ kernel/protocols/mesh/mesh.mk
@@ -0,0 +1,17 @@
+NAME := mesh
+
+$(NAME)_TYPE := kernel
+GLOBAL_INCLUDES += include
+GLOBAL_DEFINES += CONFIG_AOS_MESH
+
+ifeq ($(HOST_ARCH), linux)
+LIB_DIR := linux
+else ifeq ($(HOST_ARCH), ARM968E-S)
+LIB_DIR := arm968es
+else ifeq ($(HOST_ARCH), xtensa)
+LIB_DIR := xtensa
+else
+$(error "not find correct platform!")
+endif
+
+$(NAME)_PREBUILT_LIBRARY := lib/$(LIB_DIR)/libmesh.a
